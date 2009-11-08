f6635b4e78bdbe38b70756893f14f869d7979923
Lorenzo Pisani <zeelot3k@gmail.com>
2009-11-07 18:00:38 -0700

    adding assets system with the initial styles.css

--- themes/default/views/templates/default.php
+++ themes/default/views/templates/default.php
@@ -1,1 +1,16 @@
-Default template
+<?php
+/**
+ * Default Template
+ */
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<?php assets::render(); ?>
+<title>kocode</title>
+</head>
+
+<body>
+    <div>CONTENT HERE!! YAY!!!</div>
+</body>
+</html>
