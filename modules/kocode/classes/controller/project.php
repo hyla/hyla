357d409ca994458ec85bad994568fd1946f0b0a1
Woody Gilk <woody.gilk@kohanaphp.com>
2009-11-09 14:49:12 +0800

    Removed kocode module, preparing for cleanup

--- modules/kocode/classes/controller/project.php
+++
@@ -1,9 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class Controller_Project extends Controller_Website {
-
-	public function action_list()
-	{
-		$this->template->content = View::factory('project/list');
-	}
-}
--- modules/kocode/classes/controller/website.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-abstract class Controller_Website extends Controller_Template {
-
-	public $template = 'templates/default';
-	
-}
