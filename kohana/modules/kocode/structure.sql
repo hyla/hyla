cc5f46e0340ddcf2538d33b922d4b48f54b8a775
Lorenzo Pisani <zeelot3k@gmail.com>
2009-12-26 16:07:40 -0700

    fixing structure.sql

--- kohana/modules/kocode/structure.sql
+++ kohana/modules/kocode/structure.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 26, 2009 at 01:51 PM
+-- Generation Time: Dec 26, 2009 at 04:05 PM
 -- Server version: 5.1.37
 -- PHP Version: 5.2.10-2ubuntu6.3
 
@@ -15,22 +15,24 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- Table structure for table `plugins`
 --
 
-CREATE TABLE IF NOT EXISTS `plugins` (
+CREATE TABLE `plugins` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   `title` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
   `enabled` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`,`name`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 --
 -- Table structure for table `projects`
 --
 
-CREATE TABLE IF NOT EXISTS `projects` (
+CREATE TABLE `projects` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   `title` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
   `description` text COLLATE utf8_unicode_ci,
-  PRIMARY KEY (`id`,`name`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
