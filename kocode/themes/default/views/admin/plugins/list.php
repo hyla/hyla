beecb4f200cd26a19ae371d9e86eef0d620a2536
Lorenzo Pisani <zeelot3k@gmail.com>
2009-12-26 17:20:24 -0700

    basic list of plugins

--- kocode/themes/default/views/admin/plugins/list.php
+++ kocode/themes/default/views/admin/plugins/list.php
@@ -6,3 +6,23 @@
  *
  */
 ?>
+<table id="plugin_table">
+	<thead>
+		<tr>
+			<th>ID</th>
+			<th>Name</th>
+			<th>Title</th>
+			<th>Status</th>
+		</tr>
+	</thead>
+	<tbody>
+	<?php foreach($plugins as $plugin): ?>
+		<tr>
+			<td><?php echo $plugin->id; ?></td>
+			<td><?php echo $plugin->name; ?></td>
+			<td><?php echo $plugin->title; ?></td>
+			<td><?php echo ($plugin->enabled)? 'Enabled':'Disabled'; ?></td>
+		</tr>
+	<?php endforeach; ?>
+	</tbody>
+</table>
