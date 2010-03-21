fd2f5141c4310aa2d006959a53d64692c4080c33
Lorenzo Pisani <zeelot@zeelot-mint-8.(none)>
2010-03-21 12:31:44 -0700

    switching models to ORM

--- kohana/modules/kocode/classes/model/plugin.php
+++ kohana/modules/kocode/classes/model/plugin.php
@@ -1,23 +1,5 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Model_Plugin extends Sprig {
-
-	protected function _init()
-	{
-		$this->_fields += array(
-			'id' => new Sprig_Field_Auto,
-			'name' => new Sprig_Field_Char(array(
-				'primary' => TRUE,
-				'unique' => TRUE,
-				'max_length' => 32,
-			)),
-			'title' => new Sprig_Field_Char(array(
-				'max_length' => 64,
-			)),
-			'enabled' => new Sprig_Field_Boolean(array(
-				'default' => FALSE,
-			)),
-		);
-	}
+class Model_Plugin extends ORM {
 
 } // End Plugin
--- kohana/modules/kocode/classes/model/project.php
+++ kohana/modules/kocode/classes/model/project.php
@@ -1,17 +1,5 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Model_Project extends Sprig {
-
-	protected function _init()
-	{
-		$this->_fields += array(
-			'id' => new Sprig_Field_Auto,
-			'name' => new Sprig_Field_Char(array(
-				'max_length' => '32',
-				'unique' => TRUE,
-			)),
-			'description' => new Sprig_Field_Text,
-		);
-	}
+class Model_Project extends ORM {
 
 } // End Project
