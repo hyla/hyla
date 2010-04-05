f346c2da5e90757664eb81c9f7681a947b0882e7
Lorenzo Pisani <zeelot3k@gmail.com>
2010-04-04 22:49:26 -0700

    ok so the routes are fairly useful =P adding the project ones back in (a route for project actions that do not have a name (create/list) and one for actions with names (the rest of CRUD operations))

--- kohana/modules/kocode/init.php
+++ kohana/modules/kocode/init.php
@@ -15,8 +15,19 @@ unset($modules, $enabled);
 
 Route::set('kocode-admin', 'admin(/<controller>(/<action>(/<id>)))')
 	->defaults(array(
-		'controller'	=> 'main',
-		'directory'		=> 'admin',
+		'controller' => 'main',
+		'directory'  => 'admin',
+	));
+
+Route::set('project/general', 'project(/<action>)', array('action' => '(?:list|create)'))
+	->defaults(array(
+		'controller' => 'project',
+		'action'     => 'list',
+	));
+Route::set('project/crud', 'project/<name>(/<action>)', array('action' => '(?:details|edit|delete|archive)'))
+	->defaults(array(
+		'controller' => 'project',
+		'action'     => 'details',
 	));
 
 Route::set('kocode-main', '(<controller>(/<action>(/<id>)))')
