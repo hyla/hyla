cd7fb06f18e8fb914ba088371590e639e60fd075
Kenny Kurtz <justlikeicarus@gmail.com>
2010-04-28 23:19:15 +0400

    Added GeShi to modules

---
+++ kohana/modules/geshi/init.php
@@ -0,0 +1,16 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+Class GeSHi_Loader {
+
+	public static function auto_load($class)
+	{
+		if (strtolower($class) === 'geshi')
+		{
+			require Kohana::find_file('vendor', 'geshi/geshi');
+			return TRUE;
+		}
+		return FALSE;
+	}
+}
+/* Set autoloader for GeSHi */
+spl_autoload_register(array('GeSHi_Loader', 'auto_load'));
