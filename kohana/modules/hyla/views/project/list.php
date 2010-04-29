850420e695f5097dd9362cef697263582bd89772
Kenny Kurtz <JustLikeIcarus@gmail.com>
2010-04-28 20:55:29 -0400

    Test geshi code in project/list view

--- kohana/modules/hyla/views/project/list.php
+++ kohana/modules/hyla/views/project/list.php
@@ -3,6 +3,19 @@
 <?php if( ! count($projects)): ?>
 
 	<p>You have no projects</p>
+	<?php
+$source = '$foo = 45;
+	for ( $i = 1; $i < $foo; $i++ ){
+		echo "$foo\n";  --$foo;
+	}';
+	$language = 'php';
+
+	$geshi = new GeSHi($source, $language);
+	$geshi->enable_line_numbers(GESHI_NORMAL_LINE_NUMBERS);
+
+	echo $geshi->parse_code();
+
+	       ?>
 
 <?php else: ?>
 	<?php foreach($projects as $project): ?>
