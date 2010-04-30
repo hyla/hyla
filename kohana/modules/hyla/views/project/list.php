7644095e882f253ba376aba88e559ff2f2c49209
Kenny Kurtz <JustLikeIcarus@gmail.com>
2010-04-30 19:34:37 -0400

    Removed geshi example code from project/list view

--- kohana/modules/hyla/views/project/list.php
+++ kohana/modules/hyla/views/project/list.php
@@ -3,20 +3,6 @@
 <?php if( ! count($projects)): ?>
 
 	<p>You have no projects</p>
-	<?php
-$source = '$foo = 45;
-	for ( $i = 1; $i < $foo; $i++ ){
-		echo "$foo\n";  --$foo;
-	}';
-	$language = 'php';
-
-	$geshi = new GeSHi($source, $language);
-	$geshi->enable_line_numbers(GESHI_FANCY_LINE_NUMBERS, 2);
-	$geshi->set_line_style('background: #fcfcfc;', 'background: #f0f0f0;');
-	
-	echo $geshi->parse_code();
-
-	       ?>
 
 <?php else: ?>
 	<?php foreach($projects as $project): ?>
