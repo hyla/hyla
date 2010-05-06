a86d82933c3453bb055b64496638975147650731
Kenny <justlikeicarus@gmail.com>
2010-05-06 12:22:42 -0700

    Migration 2, changed to shorter table options method.

--- kohana/application/migrations/2_tickets_comments_statuses_labels.php
+++ kohana/application/migrations/2_tickets_comments_statuses_labels.php
@@ -2,80 +2,61 @@
 
 class TicketsCommentsStatusesLabels extends Doctrine_Migration_Base
 {
-  public function up()
-  {
-    // Types
-    $int     = array('type' => 'integer', 'length' => 8);
-    $varchar = array('type' => 'varchar', 'length' => 255);
-    $text    = array('type' => 'text');
+	public function up()
+	{
+		// Types
+		$int     = array('type' => 'integer', 'length' => 8);
+		$varchar = array('type' => 'varchar', 'length' => 255);
+		$text    = array('type' => 'text');
 
-    // Attributes
-    $notnull  = array('notnull' => TRUE);
-    $unsigned = array('unsigned' => TRUE);
-    $pk       = array('primary' => TRUE);
-    $ai       = array('autoincrement' => TRUE);
+		// Attributes
+		$notnull  = array('notnull' => TRUE);
+		$unsigned = array('unsigned' => TRUE);
+		$pk       = array('primary' => TRUE);
+		$ai       = array('autoincrement' => TRUE);
+		
+		parent::setDefaultTableOptions(array('type' => 'INNODB', 'charset' => 'utf8'));
+		
+		$this->createTable('tickets', array
+		(
+			'id'          => $int + $notnull + $unsigned + $ai + $pk,
+			'user_id'     => $int + $notnull + $unsigned,
+			'project_id'  => $int + $notnull + $unsigned,
+			'status_id'   => $int + $notnull + $unsigned,
+			'title'       => $varchar,
+			'description' => $text,
+			'created_on'  => $int + $notnull + $unsigned,
+			'last_update' => $int + $notnull + $unsigned,
+		));
 
-    $this->createTable('tickets', array
-    (
-        'id'          => $int + $notnull + $unsigned + $ai + $pk,
-        'user_id'     => $int + $notnull + $unsigned,
-        'project_id'  => $int + $notnull + $unsigned,
-        'status_id'   => $int + $notnull + $unsigned,
-        'title'       => $varchar,
-        'description' => $text,
-        'created_on'  => $int + $notnull + $unsigned,
-        'last_update' => $int + $notnull + $unsigned,
-    ),
-    array
-    (
-        'type'     => 'INNODB',
-        'charset'  => 'utf8'
-    ));
+		$this->createTable('comments', array
+		(
+			'id' => $int + $notnull + $unsigned + $ai + $pk,
+			'ticket_id' => $int + $notnull + $unsigned,
+			'user_id' => $int + $notnull + $unsigned,
+			'content' => $text,
+			'created_on' => $int + $notnull + $unsigned,
+			'last_update' => $int + $notnull + $unsigned,
+		));
 
-    $this->createTable('comments', array(
-                                    'id' => $int + $notnull + $unsigned + $ai + $pk,
-                                    'ticket_id' => $int + $notnull + $unsigned,
-                                    'user_id' => $int + $notnull + $unsigned,
-                                    'content' => $text,
-                                    'created_on' => $int + $notnull + $unsigned,
-                                    'last_update' => $int + $notnull + $unsigned,
-                                  ),
-                                  array(
-                                    'type'     => 'INNODB',
-                                    'charset'  => 'utf8'
-                                  )
-                      );
+		$this->createTable('statuses', array
+		(
+			'id' => $int + $notnull + $unsigned + $ai + $pk,
+			'name' => $varchar
+		));
 
-    $this->createTable('statuses', array(
-                                    'id' => $int + $notnull + $unsigned + $ai + $pk,
-                                    'name' => $varchar
-                                  ),
-                                  array(
-                                    'type'     => 'INNODB',
-                                    'charset'  => 'utf8'
-                                  )
-                      );
+		$this->createTable('labels', array
+		(
+			'id' => $int + $notnull + $unsigned + $ai + $pk,
+			'name' => $varchar,
+			'color' => $varchar
+		));
 
-    $this->createTable('labels', array(
-                                    'id' => $int + $notnull + $unsigned + $ai + $pk,
-                                    'name' => $varchar,
-                                    'color' => $varchar
-                                  ),
-                                  array(
-                                    'type'     => 'INNODB',
-                                    'charset'  => 'utf8'
-                                  )
-                      );
-
-    $this->createTable('tickets_labels', array(
-                                    'ticket_id' => $int + $notnull + $unsigned,
-                                    'label_id' => $int + $notnull + $unsigned
-                                  ),
-                                  array(
-                                    'type'     => 'INNODB',
-                                    'charset'  => 'utf8'
-                                  )
-                      );
+		$this->createTable('tickets_labels', array
+		(
+			'ticket_id' => $int + $notnull + $unsigned,
+			'label_id' => $int + $notnull + $unsigned
+		));
 
     }
 
