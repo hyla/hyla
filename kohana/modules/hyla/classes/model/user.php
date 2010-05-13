1706e1e56d834f47e7a826cfe9f6505d14103948
Kenny <justlikeicarus@gmail.com>
2010-05-13 09:29:07 -0700

    Extended Auth_User Model

--- kohana/modules/hyla/classes/model/user.php
+++ kohana/modules/hyla/classes/model/user.php
@@ -1,17 +1,8 @@
  <?php defined('SYSPATH') or die('No direct script access.');
 
-class Model_User extends ORM
+class Model_User extends Model_Auth_User
 {
 	protected $_has_many = array('projects' => array(), 'tickets' => array(), 'comments' => array());
 	
-	public function rules()
-	{
-		$rules = array
-		(
-			'username' => array('not_empty' => array()),
-			'email' => array('not_empty' => array(), 'email' => array()),
-			'password' => array('not_empty' => array())
-		);
-		return $rules;
-	}	
+	
 }
