5524bee1bc810ba87f0bb9b98f8d9ab0ac4a8e6d
Kenny Kurtz <JustLikeIcarus@gmail.com>
2010-05-15 11:24:48 -0400

    Updated migration 5

--- kohana/application/migrations/5_rename_user_last_update.php
+++ kohana/application/migrations/5_rename_user_last_update.php
@@ -4,6 +4,8 @@ class RenameColumn extends Doctrine_Migration_Base
 {
 	public function up()
 	{
+		$options = array('length' => 8, 'default' => 0);
+		$this->changeColumn('users', 'last_update', 'int', $options);
 		$this->renameColumn('users', 'last_update', 'last_login');
 		$this->addColumn('users', 'logins', 'integer', 8, array('unsigned' => TRUE));
 	}
