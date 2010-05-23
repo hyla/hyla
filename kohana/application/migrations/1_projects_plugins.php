c3e5341df08f10e3ede5eb61dbb907ece9a10241
Lorenzo Pisani <zeelot3k@gmail.com>
2010-05-23 16:11:12 -0700

    I gotta admit... I almost didn't resist the urge to kill myself

--- kohana/application/migrations/1_projects_plugins.php
+++ kohana/application/migrations/1_projects_plugins.php
@@ -2,71 +2,45 @@
 
 class Projects_Plugins extends Doctrine_Migration_Base
 {
-  public function up()
-  {
+	public function up()
+	{
+		// Types
+		$int      = array('type' => 'integer', 'length' => 8);
+		$varchar  = array('type' => 'varchar', 'length' => 255);
+		$string32 = array('type' => 'string' , 'length' => 32);
+		$string64 = array('type' => 'string' , 'length' => 64);
+		$text     = array('type' => 'text');
+		$bool     = array('type' => 'integer', 'length' => 1, 'default' => 0);
 
-    $this->createTable('projects', array(
-                                    'id' => array(
-                                      'type'          => 'integer',
-                                      'length'        => 11,
-                                      'notnull'       => true, 
-                                      'unsigned'      => true,
-                                      'primary'       => true,
-                                      'autoincrement' => true
-                                    ), 
-                                    'name' => array(
-                                      'type'    => 'string', 
-                                      'length'  => 32
-                                    ),
-                                    'title' => array(
-                                      'type'    => 'string', 
-                                      'length'  => 64
-                                    ),
-                                    'description' => array(
-                                      'type' => 'text'
-                                    )
-                                  ),
-                                  array(
-                                    'type'     => 'INNODB',
-                                    'charset'  => 'utf8'
-                                  )
-                      );
+		// Attributes
+		$notnull  = array('notnull'       => TRUE);
+		$unsigned = array('unsigned'      => TRUE);
+		$pk       = array('primary'       => TRUE);
+		$ai       = array('autoincrement' => TRUE);
 
-    $this->createTable('plugins', array(
-                                   'id' => array(
-                                      'type'          => 'integer',
-                                      'length'        => 11,
-                                      'notnull'       => true, 
-                                      'unsigned'      => true,
-                                      'primary'       => true,
-                                      'autoincrement' => true
-                                    ),
-                                    'name' => array(
-                                      'type'    => 'string', 
-                                      'length'  => 32
-                                    ),
-                                    'title' => array(
-                                      'type'    => 'string', 
-                                      'length'  => 64
-                                    ),
-                                    'enabled' => array(
-                                      'type'      => 'integer', 
-                                      'length'    => 1,
-                                      'notnull'   => true, 
-                                      'unsigned'  => true,
-                                      'default'   => 0
-                                    ),
-                                  ),
-                                  array(
-                                    'type'     => 'INNODB',
-                                    'charset'  => 'utf8'
-                                  )
-                      ); 
-    }
+		// Table Attributes
+		$innodb = array('type'     => 'INNODB');
+		$utf8   = array('charset'  => 'utf8');
 
-    public function down()
-    {
-      $this->dropTable('projects');
-      $this->dropTable('plugins');
-    }
+		$this->createTable('projects', array
+		(
+			'id'          => $int + $notnull + $unsigned + $ai + $pk,
+			'name'        => $string32,
+			'title'       => $string64,
+			'description' => $text
+		), $innodb + $utf8);
+
+		$this->createTable('plugins', array(
+			'id'      => $int + $notnull + $unsigned + $ai + $pk,
+			'name'    => $string32,
+			'title'   => $string64,
+			'enabled' => $bool + $notnull + $unsigned,
+		), $innodb + $utf8);
+	}
+
+	public function down()
+	{
+		$this->dropTable('projects');
+		$this->dropTable('plugins');
+	}
 }
