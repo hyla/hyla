a1828d428a7ff3c5f59ab491581671c2bd595eac
Lorenzo Pisani <zeelot3k@gmail.com>
2010-05-31 12:47:03 -0700

    argh turn on invisibles! >_<

--- kohana/modules/hyla/classes/model/user.php
+++ kohana/modules/hyla/classes/model/user.php
@@ -6,10 +6,10 @@ class Model_User extends Model_Auth_User
 	(
 		'user_tokens' => array('model' => 'user_token'),
 		'roles'       => array('model' => 'role', 'through' => 'roles_users'),
-		'projects'    => array(), 
-		'tickets'     => array(), 
-		'comments'    => array()
+		'projects'    => array(),
+		'tickets'     => array(),
+		'comments'    => array(),
 	);
-	
+
 	protected $_created_column = array('created_on' => TRUE);
 }
