1882028524d9f184f3886a7e4c546b5ed40374ba
Lorenzo Pisani <zeelot3k@gmail.com>
2011-01-09 16:20:34 -0700

    removing all views models and template controllers, getting ready for some refactoring

--- kohana/modules/hyla/classes/controller/template/hyla.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class Controller_Template_Hyla extends Controller_Template {
-
-	public $template = 'templates/default';
-
-} // End Template_Hyla
--- kohana/modules/hyla/classes/controller/template/hyla/admin.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class Controller_Template_Hyla_Admin extends Controller_Template_Hyla {
-
-	public $template = 'templates/admin';
-
-} // End Template_Hyla_Admin
