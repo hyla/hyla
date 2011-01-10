f18a9084434a7dc5098d6526b923420ab5c0997c
Lorenzo Pisani <zeelot3k@gmail.com>
2011-01-09 17:21:50 -0700

    cleaning up hyla init file

--- kohana/modules/hyla/init.php
+++ kohana/modules/hyla/init.php
@@ -1,31 +1,15 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-Route::set('hyla-admin', 'admin(/<controller>(/<action>(/<id>)))')
+Route::set('hyla/project', 'project(/<action>)', array('action' => '(?:list|create)'))
 	->defaults(array(
-		'controller' => 'main',
-		'directory'  => 'admin',
-	));
-
-Route::set('project/general', 'project(/<action>)', array('action' => '(?:list|create)'))
-	->defaults(array(
+		'directory'  => 'page',
 		'controller' => 'project',
 		'action'     => 'list',
 	));
 
-Route::set('project/crud', 'project/<name>(/<action>)', array('action' => '(?:details|home|delete|archive)'))
+Route::set('hyla/project/crud', 'project/<name>(/<action>)', array('action' => '(?:details|home|delete|archive)'))
 	->defaults(array(
+		'directory'  => 'page',
 		'controller' => 'project',
 		'action'     => 'home',
-	));
-
-Route::set('project/settings', 'project/<name>/settings(/<action>)', array('action' => '(?:main|members)'))
-	->defaults(array(
-		'directory'  => 'project',
-		'controller' => 'settings',
-		'action'     => 'main',
-	));
-
-Route::set('hyla-main', '(<controller>(/<action>(/<id>)))')
-	->defaults(array(
-		'controller' => 'main',
 	));
