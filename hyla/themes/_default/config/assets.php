303a6381bcea059c65d172a01370cad9297bf10f
Lorenzo Pisani <zeelot3k@gmail.com>
2011-06-26 02:32:43 -0700

    adding assets config file with the default page group

---
+++ hyla/themes/_default/config/assets.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array
+(
+	'default-page' => array
+	(
+		array('style', Media::url('css/compiled/styles.css'), 'head'),
+	),
+);
