eb6ef4e5ac2b69a38a0f297e1d81d6ef9c45f66d
Lorenzo Pisani <zeelot3k@gmail.com>
2011-06-26 02:27:33 -0700

    added a way for page views to replace asset blocks in the template

--- hyla/themes/_default/classes/abstract/view/page.php
+++ hyla/themes/_default/classes/abstract/view/page.php
@@ -16,6 +16,49 @@ abstract class Abstract_View_Page extends Abstract_View_Layout {
 		return $this->title;
 	}
 
+	public function assets_head()
+	{
+		if ( ! $this->_assets)
+			return '';
+
+		$assets = '';
+		foreach ($this->_assets->get('head') as $asset)
+		{
+			$assets .= $asset."\n";
+		}
+
+		return $assets;
+	}
+
+	public function assets_body()
+	{
+		if ( ! $this->_assets)
+			return '';
+
+		$assets = '';
+		foreach ($this->_assets->get('body') as $asset)
+		{
+			$assets .= $asset;
+		}
+
+		return $assets;
+	}
+
+	public function render($template = NULL, $view = NULL, $partials = NULL)
+	{
+		$content = parent::render($template, $view, $partials);
+
+		return str_replace(array
+		(
+			'[[assets_head]]',
+			'[[assets_body]]'
+		), array
+		(
+			$this->assets_head(),
+			$this->assets_body()
+		), $content);
+	}
+
 	public function profiler()
 	{
 		return View::factory('profiler/stats');
