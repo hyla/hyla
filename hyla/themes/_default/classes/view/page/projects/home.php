cf5215b0f9ff9a988f54c30af017bb12763c70a5
Lorenzo Pisani <zeelot3k@gmail.com>
2011-07-20 22:36:00 -0700

    project home view now extends the new project view

--- hyla/themes/_default/classes/view/page/projects/home.php
+++ hyla/themes/_default/classes/view/page/projects/home.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class View_Page_Projects_Home extends Abstract_View_Page {
+class View_Page_Projects_Home extends Abstract_View_Page_Project {
 
 	public function project()
 	{
