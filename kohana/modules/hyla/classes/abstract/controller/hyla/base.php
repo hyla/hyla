9ef25f0621ecf8dd7a1ed3535e958d130e75c4f6
Lorenzo Pisani <zeelot3k@gmail.com>
2011-07-20 22:35:12 -0700

    setting up a dispatcher in the controllers

--- kohana/modules/hyla/classes/abstract/controller/hyla/base.php
+++ kohana/modules/hyla/classes/abstract/controller/hyla/base.php
@@ -7,9 +7,16 @@ abstract class Abstract_Controller_Hyla_Base extends Controller {
 	 */
 	public $view;
 
+	/**
+	 * @var object the event dispatcher for this request
+	 */
+	public $dispatcher;
+
 	public function before()
 	{
 		$this->view = $this->_request_view();
+		$this->dispatcher = Event_Dispatcher::factory()
+			->load_subscribers();
 	}
 
 	/**
@@ -23,6 +30,9 @@ abstract class Abstract_Controller_Hyla_Base extends Controller {
 		// If content is NULL, then there is no View to render
 		if ($this->view === NULL)
 			throw new Kohana_View_Exception('There was no View created for this request.');
+
+		$this->view
+			->set('dispatcher', $this->dispatcher);
 
 		$this->response->body($this->view);
 	}
