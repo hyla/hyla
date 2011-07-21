3d4f30658972bbec0baeb462d579d17639c6e934
Lorenzo Pisani <zeelot3k@gmail.com>
2011-07-20 22:34:22 -0700

    creating an abstract project view to trigger sub navigation event

---
+++ hyla/themes/_default/classes/abstract/view/page/project.php
@@ -0,0 +1,21 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Contains methods useful to project pages
+ *
+ * @package    Hyla
+ * @category   Kostache
+ * @author     Synapse Studios
+ */
+abstract class Abstract_View_Page_Project extends Abstract_View_Page {
+
+	public function sub_navigation()
+	{
+		$navigation = new Model_Navigation;
+		$arguments = array('project' => $this->project);
+
+		// Trigger this event so plugins can alter the navigation before it's rendered
+		$this->dispatcher->trigger('hyla:project.sub-nav', new Event($navigation, $arguments));
+
+		return $navigation->as_array();
+	}
+}
