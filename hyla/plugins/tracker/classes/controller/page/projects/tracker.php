710a091f7a5008440b08fa6221add1ec59f00fea
Lorenzo Pisani <zeelot3k@gmail.com>
2011-07-25 23:56:49 -0700

    fixing up the ticket list page

--- hyla/plugins/tracker/classes/controller/page/projects/tracker.php
+++ hyla/plugins/tracker/classes/controller/page/projects/tracker.php
@@ -2,13 +2,12 @@
 
 class Controller_Page_Projects_Tracker extends Abstract_Controller_Hyla_Page {
 
-	public function action_home()
+	public function action_list()
 	{
 		$this->view
 			->bind('project', $project);
 
-		$config = Kohana::config('couchdb');
-		$project = Couch_Model::factory('project', new Sag($config->host, $config->port))
+		$project = Couch_Model::factory('project', $this->couchdb)
 			->find_by_slug($this->request->param('slug'));
 
 		if ( ! $project->loaded())
