0d5da555eea3e2597f9724d5c97be03b815826f5
Lorenzo Pisani <zeelot3k@gmail.com>
2011-08-02 00:16:46 -0700

    adding the default rabbitmq config

---
+++ kohana/application/config/rabbitmq.php
@@ -0,0 +1,14 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+
+	'servers' => array(
+		'local' => array(
+			'host'     => 'localhost',
+			'port'     => '5672',
+			'vhost'    => 'hyla',
+			'login'    => 'guest',
+			'password' => 'guest',
+		),
+	),
+);
