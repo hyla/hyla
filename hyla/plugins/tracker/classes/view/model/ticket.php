704daf23604675bf171ea3679fe3d5de957288d9
Lorenzo Pisani <zeelot3k@gmail.com>
2011-08-17 23:43:03 -0700

    added formatted versions of the updated/created times

--- hyla/plugins/tracker/classes/view/model/ticket.php
+++ hyla/plugins/tracker/classes/view/model/ticket.php
@@ -30,4 +30,29 @@ class View_Model_Ticket extends View_Model {
 
 		return $data;
 	}
+
+	public function created_on_date_time()
+	{
+		if ( ! $this->_model->get('created_on'))
+			return NULL;
+
+		return date('m/d/Y h:i a', $this->_model->get('created_on'));
+	}
+
+	public function updated_on_date_time()
+	{
+		if ( ! $this->_model->get('updated_on'))
+			return NULL;
+
+		return date('m/d/Y h:i a', $this->_model->get('updated_on'));
+	}
+
+	public function url()
+	{
+		return Route::url('hyla-tracker', array(
+				'slug'   => $this->_model->get_project()->get('slug'),
+				'action' => 'view',
+				'ticket' => $this->_model->get('_id'),
+			));
+	}
 }
