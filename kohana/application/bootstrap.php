6a9b6d327c877ec7a96b9517a586d6a56f0e179b
Lorenzo Pisani <zeelot3k@gmail.com>
2011-08-22 20:52:37 -0700

    code style

--- kohana/application/bootstrap.php
+++ kohana/application/bootstrap.php
@@ -111,19 +111,19 @@ Kohana::$config->attach(new Config_File);
  */
 Kohana::modules(array(
 	// temporary until theme is taken from user/site settings
-	'theme'          => HYLAPATH.'themes/_default',    // default hyla theme
-	'tracker'        => HYLAPATH.'plugins/tracker',    // Tracker Plugin
-	'hyla'           => HYLAPATH.'core',               // Core hyla module
-	'acl'            => MODPATH.'acl',                 // ACL Module
-	'assets'         => MODPATH.'assets',              // Asset Library
-	'media'          => MODPATH.'media',               // Kohana CFS Media Module
-	'kostache'       => MODPATH.'kostache',            // View Classes
-	'minion'         => MODPATH.'minion',              // CLI module
-	'unittest'       => MODPATH.'unittest',            // PHPUnit support
-	'events'         => MODPATH.'event-dispatcher',    // Event Dispatcher,
-	'yform'          => MODPATH.'yform',               // Form Generation
+	'theme'          => HYLAPATH.'themes/_default',      // default hyla theme
+	'tracker'        => HYLAPATH.'plugins/tracker',      // Tracker Plugin
+	'hyla'           => HYLAPATH.'core',                 // Core hyla module
+	'acl'            => MODPATH.'acl',                   // ACL Module
+	'assets'         => MODPATH.'assets',                // Asset Library
+	'media'          => MODPATH.'media',                 // Kohana CFS Media Module
+	'kostache'       => MODPATH.'kostache',              // View Classes
+	'minion'         => MODPATH.'minion',                // CLI module
+	'unittest'       => MODPATH.'unittest',              // PHPUnit support
+	'events'         => MODPATH.'event-dispatcher',      // Event Dispatcher,
+	'yform'          => MODPATH.'yform',                 // Form Generation
 	'config-couchdb' => MODPATH.'config-driver-couchdb', // Config driver for CouchDB
-	'tasks-media'    => MODPATH.'minion-tasks-media',
+	'tasks-media'    => MODPATH.'minion-tasks-media',    // Media related tasks
 	'userguide'      => MODPATH.'userguide',             // Documentation
 ));
 
