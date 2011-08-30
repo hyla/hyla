207ac66dc2f943fe96d46cd56d96168124d6d680
Lorenzo Pisani <zeelot3k@gmail.com>
2011-08-30 00:58:52 -0700

    changing .couchapprc save path

--- hyla/core/classes/view/hyla/installer/couchapp/couchapprc.php
+++ hyla/core/classes/view/hyla/installer/couchapp/couchapprc.php
@@ -1,4 +1,3 @@
-http://dev.vm:5984/hyla
 <?php defined('SYSPATH') or die('No direct script access.');
 
 class View_Hyla_Installer_CouchApp_CouchAppRC extends Kostache {
@@ -12,6 +11,6 @@ class View_Hyla_Installer_CouchApp_CouchAppRC extends Kostache {
 
 	public function save_path()
 	{
-		return HYLAPATH.'core/couchapp/.couchapprc';
+		return APPPATH.'media/couchapp/.couchapprc';
 	}
 }
