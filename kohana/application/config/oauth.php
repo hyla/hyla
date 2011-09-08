67246b99c7a8070ffd7ebf9e327bdc40e0f9a362
Lorenzo Pisani <zeelot3k@gmail.com>
2011-09-07 23:12:28 -0700

    committing my dev oauth config

---
+++ kohana/application/config/oauth.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+	'github' => array(
+		'client_id'    => '01bbc9d4d4dce5edcf66',
+		'secret'       => '39ba7112ebc36d4ad980cf0c878190f291dbb888',
+		'oauth_dialog' => 'https://github.com/login/oauth/authorize',
+		'oauth_token'  => 'https://github.com/login/oauth/access_token',
+		'api_url'      => 'https://github.com/api/v2/json/',
+	),
+);
