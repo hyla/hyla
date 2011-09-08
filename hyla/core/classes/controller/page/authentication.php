6bdfde4eff99fe9d1592ca7bd54f5c64c69feefa
Lorenzo Pisani <zeelot3k@gmail.com>
2011-09-07 22:57:02 -0700

    implementing the api oauth in the authentication controller

--- hyla/core/classes/controller/page/authentication.php
+++ hyla/core/classes/controller/page/authentication.php
@@ -27,7 +27,8 @@ class Controller_Page_Authentication extends Abstract_Controller_Hyla_Page {
 
 			// Log the user in
 			Cookie::set('auth', $user->get('_id'));
-			$this->request->redirect(Route::url('hyla/home'));
+			// Log the user into the API (I guess)
+			$this->request->redirect(Route::url('hyla/log_in', array('action' => 'hyla')));
 		}
 		else
 		{
@@ -35,6 +36,56 @@ class Controller_Page_Authentication extends Abstract_Controller_Hyla_Page {
 			$query = array(
 				'client_id' => $config['client_id'],
 				'redirect_uri' => URL::site($this->request->uri()),
+			);
+
+			$this->request->redirect($url.'?'.http_build_query($query));
+		}
+	}
+
+	public function action_hyla()
+	{
+		$config = Kohana::$config->load('oauth2')->consumer['web'];
+
+		if ($this->request->query('code'))
+		{
+			// User sent back with a code
+			$url = $config['token_uri'];
+			$query = array(
+				'client_id'     => $config['client_id'],
+				'client_secret' => $config['client_secret'],
+
+				'code'          => $this->request->query('code'),
+			);
+
+			$request = Request::factory($url)
+				->method(Request::POST)
+				->post(array(
+					'grant_type'    => 'authorization_code',
+					'client_id'     => $config['client_id'],
+					'client_secret' => $config['client_secret'],
+					'code'          => $this->request->query('code'),
+					'redirect_uri'  => URL::site($this->request->uri()),
+				));
+
+			$response = $request->execute();
+
+			$info = json_decode($response->body(), TRUE);
+
+			$token_type = $info['token_type'];
+			$access_token = $info['access_token'];
+			$refresh_token = $info['refresh_token'];
+
+			$token = Model_OAuth2_User_Token::create_token('web', $token_type, $access_token, $this->auth->get('_id'), $refresh_token);
+
+			$this->request->redirect(Route::url('hyla/home'));
+		}
+		else
+		{
+			$url = $config['authorize_uri'];
+			$query = array(
+				'client_id' => $config['client_id'],
+				'redirect_uri' => URL::site($this->request->uri()),
+				'response_type' => 'code',
 			);
 
 			$this->request->redirect($url.'?'.http_build_query($query));
