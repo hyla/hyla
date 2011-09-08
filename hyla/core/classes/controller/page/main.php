ffb6fd06f7c34bf4820ae97d7fd19e310f51dd6c
Lorenzo Pisani <zeelot3k@gmail.com>
2011-09-07 22:56:44 -0700

    home page is back to normal

--- hyla/core/classes/controller/page/main.php
+++ hyla/core/classes/controller/page/main.php
@@ -2,13 +2,5 @@
 
 class Controller_Page_Main extends Abstract_Controller_Hyla_Page {
 
-	public function action_home() {
-		$oauth_client = OAuth2_Consumer::factory('web', OAuth2::GRANT_TYPE_CLIENT_CREDENTIALS);
-
-		$request = Request::factory('api/test');
-
-		// Execute the request, via the client.
-		$response = json_decode($oauth_client->execute($request));
-		echo Debug::vars($response);
-	}
+	public function action_home() {}
 }
