761a7d2ceec57814a3071aaf52f487fb394296fe
Lorenzo Pisani <zeelot3k@gmail.com>
2011-09-10 14:51:09 -0700

    creating the stub tickets api actions

---
+++ hyla/plugins/tracker/classes/controller/api/tracker/tickets.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_API_Tracker_Tickets extends Abstract_Controller_Hyla_API {
+
+	public function action_get_collection() {}
+	public function action_get_ticket() {}
+	public function action_put_ticket() {}
+	public function action_delete_ticket() {}
+}
