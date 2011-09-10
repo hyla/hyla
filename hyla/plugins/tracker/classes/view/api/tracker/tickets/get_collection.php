65682ffe16d6912a8eea545e94bbe503db943d7a
Lorenzo Pisani <zeelot3k@gmail.com>
2011-09-10 15:38:15 -0700

    GET /tickets returns more information now

--- hyla/plugins/tracker/classes/view/api/tracker/tickets/get_collection.php
+++ hyla/plugins/tracker/classes/view/api/tracker/tickets/get_collection.php
@@ -4,7 +4,21 @@ class View_API_Tracker_Tickets_Get_Collection extends Abstract_View_API {
 
 	public function collection()
 	{
-		return Couch_Model::factory('ticket', $this->couchdb)
-			->find_all(TRUE);
+		$tickets = Couch_Model::factory('ticket', $this->couchdb)
+			->find_all();
+
+		$data = array();
+		foreach ($tickets as $ticket)
+		{
+			$ticket = View_Model::factory($ticket);
+			$item = $ticket->document();
+			$item['url'] = $ticket->url();
+			$item['created_on_date_time'] = $ticket->created_on_date_time();
+			$item['updated_on_date_time'] = $ticket->updated_on_date_time();
+
+			$data[] = $item;
+		}
+
+		return $data;
 	}
 }
