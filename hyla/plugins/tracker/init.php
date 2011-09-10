4277b6e003c0a0a1b27973de8f27a93f0941d3ea
Lorenzo Pisani <zeelot3k@gmail.com>
2011-09-10 14:50:08 -0700

    adding a route for the tickets api

--- hyla/plugins/tracker/init.php
+++ hyla/plugins/tracker/init.php
@@ -6,3 +6,19 @@ Route::set('hyla-tracker', 'projects/<slug>/tracker(/<action>(/<ticket>))')
 		'controller' => 'tracker',
 		'action'     => 'list',
 	));
+
+Route::set('hyla/api/tickets', 'api/tickets(/<id>)', array('id' => '\d'))
+	->callback(function($route, $uri, $params, $request) {
+		$params['action'] = Valid::not_empty($params['id'])
+			? 'ticket'
+			: 'collection';
+
+		return $params;
+	})
+	->callback('API_Router::prefix_request_method')
+	->defaults(array(
+		'directory'  => 'api/tracker',
+		'controller' => 'tickets',
+		'action'     => NULL,
+		'id'         => NULL,
+	));
