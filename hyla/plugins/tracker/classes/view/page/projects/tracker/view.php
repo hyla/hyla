44a60690d38f74e0fec60f07c30e03da4085ad4b
Lorenzo Pisani <zeelot3k@gmail.com>
2011-09-11 13:55:22 -0700

    switch tracker ticket/view page to use API

--- hyla/plugins/tracker/classes/view/page/projects/tracker/view.php
+++ hyla/plugins/tracker/classes/view/page/projects/tracker/view.php
@@ -2,20 +2,11 @@
 
 class View_Page_Projects_Tracker_View extends Abstract_View_Page_Project {
 
-	public function ticket()
-	{
-		return View_Model::factory($this->ticket);
-	}
-
 	public function update_url()
 	{
 		if ( ! $this->auth->can('updateTicket'))
 			return NULL;
 
-		return Route::url('hyla-tracker', array(
-			'action' => 'update',
-			'slug'   => $this->project->get('slug'),
-			'ticket' => $this->ticket->get('_id'),
-		));
+		return $this->ticket['update_url'];
 	}
 }
