96e8415f57eec5ada4cf62547f5493fc6e2f7cc1
Lorenzo Pisani <zeelot3k@gmail.com>
2011-09-14 23:00:41 -0700

    initial dependencies config with KRabbit

---
+++ hyla/core/config/dependencies.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+	'krabbit' => array(
+		'_settings' => array(
+			'class'       => 'KRabbit',
+			'constructor' => 'factory',
+			'shared'      => TRUE,
+		),
+	),
+);
