5918bac7e04e4c00603b08d3b45c99494e1b7191
Lorenzo Pisani <zeelot3k@gmail.com>
2011-10-04 00:25:07 -0700

    Implement email notifications for new projects

--- hyla/core/classes/minion/task/workers/notifier.php
+++ hyla/core/classes/minion/task/workers/notifier.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Minion_Task_Workers_Notifier extends Minion_Task {
+class Minion_Task_Workers_Notifier extends Abstract_Minion_Task {
 
 	public function execute(array $config)
 	{
@@ -32,6 +32,38 @@ class Minion_Task_Workers_Notifier extends Minion_Task {
 
 	protected function _handle_model_project_create($project_id)
 	{
-		// @TODO: Send an email to anyone that opted in
+		$project = $this->di_container->get('couch_model.project')
+			->find($project_id);
+
+		if ( ! $project->loaded())
+			return; // Project must have been deleted before we could send notifications
+
+		$mailer = $this->di_container->get('swiftmail.mailer');
+
+		$users = $this->di_container->get('couch_model.user')
+			->find_by_notification_setting('project-create.email');
+
+		if ( ! count($users))
+			return; // No users want emails about this
+
+		// Use a view to generate the email
+		$view = Kostache::factory('email/notification/project/create')
+			->set('project', $project);
+
+		$message = Swift_Message::newInstance()
+			->setSubject($view->subject())
+			->setFrom($view->from())
+			->setBody($view->render());
+
+		// Gather all the email addresses
+		$emails = array();
+		foreach ($users as $user)
+		{
+			$emails[] = $user->path('github.email');
+		}
+		// Add them using BCC for privacy
+		$message->setBcc($emails);
+
+		$mailer->send($message);
 	}
 }
