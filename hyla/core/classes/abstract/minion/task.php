b643c1f994229e2b0c22500eafee8b24fdd1c992
Lorenzo Pisani <zeelot3k@gmail.com>
2011-10-04 00:09:39 -0700

    a base minion task class that adds access to the DI Container

---
+++ hyla/core/classes/abstract/minion/task.php
@@ -0,0 +1,16 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+abstract class Abstract_Minion_Task extends Minion_Task {
+
+	/**
+	 * @var object the Dependency Container object
+	 */
+	public $di_container;
+
+	public function __construct()
+	{
+		$definitions = Dependency_Definition_List::factory()
+			->from_array(Kohana::$config->load('dependencies')->as_array());
+		$this->di_container = new Dependency_Container($definitions);
+	}
+}
