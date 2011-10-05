4b4540d7d82d39b51e99ee954c202b78d4d8be74
Lorenzo Pisani <zeelot3k@gmail.com>
2011-10-04 22:28:25 -0700

    implement email notification for ticket.create event

--- hyla/plugins/tracker/classes/subscriber/tracker/notifications.php
+++ hyla/plugins/tracker/classes/subscriber/tracker/notifications.php
@@ -6,6 +6,7 @@ class Subscriber_Tracker_Notifications implements Interface_Subscriber {
 	{
 		return array(
 			'hyla:account-settings.notifications-form' => array($this, 'notifications_form'),
+			'hyla:worker.notifier:model.ticket.create' => array($this, 'send_ticket_create_email'),
 		);
 	}
 
@@ -32,5 +33,46 @@ class Subscriber_Tracker_Notifications implements Interface_Subscriber {
 				),
 			),
 		);
+	}
+
+	public function send_ticket_create_email(Interface_Event $event)
+	{
+		// The id of the ticket
+		$id = $event->get_subject();
+		$di_container = Arr::get($event->get_arguments(), 'di_container');
+
+		$ticket = $di_container->get('couch_model.ticket')
+			->find($id);
+
+		if ( ! $ticket->loaded())
+			return; // Ticket must have been deleted before we could send notifications
+
+		$mailer = $di_container->get('swiftmail.mailer');
+
+		$users = $di_container->get('couch_model.user')
+			->find_by_notification_setting('ticket-create.email');
+
+		if ( ! count($users))
+			return; // No users want emails about this
+
+		// Use a view to generate the email
+		$view = Kostache::factory('email/notification/ticket/create')
+			->set('ticket', $ticket);
+
+		$message = Swift_Message::newInstance()
+			->setSubject($view->subject())
+			->setFrom($view->from())
+			->setBody($view->render());
+
+		// Gather all the email addresses
+		$emails = array();
+		foreach ($users as $user)
+		{
+			$emails[] = $user->path('github.email');
+		}
+		// Add them using BCC for privacy
+		$message->setBcc($emails);
+
+		$mailer->send($message);
 	}
 }
