693ba572e27fbff69be3bbc13b28e1d30c86b9b8
Lorenzo Pisani <zeelot3k@gmail.com>
2011-12-04 17:07:52 -0800

    add more software to the VM

--- vagrant/cookbooks/vagrant_main/recipes/default.rb
+++ vagrant/cookbooks/vagrant_main/recipes/default.rb
@@ -23,6 +23,16 @@ require_recipe "couchdb"
 package "couchapp"
 package "php5-xdebug"
 
+apt_repository "php-amqp" do
+  uri "http://ppa.launchpad.net/managedit/php-extensions/ubuntu"
+  distribution "oneiric"
+  components ["main"]
+  key "F9E66E70"
+  keyserver "keyserver.ubuntu.com"
+  action :add
+end
+package "php5-amqp"
+
 web_app "localhost" do
   server_name "localhost"
   docroot "/var/www/hyla"
@@ -50,4 +60,15 @@ end
 
 service "couchdb" do
   action [ :enable, :start ]
+end
+service "apache2" do
+  action :restart
+end
+
+remote_file "/usr/bin/wkhtmltopdf" do
+  source "http://zeelot.s3.amazonaws.com/cookbook-files/wkhtmltopdf-amd64-0.9.9"
+  group "root"
+  owner "root"
+  mode "0755"
+  checksum "c1047cca6bce10d3d1cf7fed4520f2f2be5be5176cba73cf550d0f87f530df3e"
 end
