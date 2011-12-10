d3c87fb0aca5c914f189fdbf5bcb7d32a0590e48
Lorenzo Pisani <zeelot3k@gmail.com>
2011-12-10 15:54:11 -0800

    remove that tab!

--- hyla/core/classes/controller/api/projects.php
+++ hyla/core/classes/controller/api/projects.php
@@ -1,7 +1,7 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 class Controller_API_Projects extends Abstract_Controller_Hyla_API {
-	
+
 	public function action_get_collection() {}
 	public function action_get_project() {}
 	public function action_put_project() {}
